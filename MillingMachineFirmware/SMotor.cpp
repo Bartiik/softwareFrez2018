55f3e9ebb0adf9a954b7062c22e8744ab2ce2b9d
Unknown <wiechec.m@gmail.com>
2018-05-23 11:14:43 +0200

    Smotor update

---
+++ MillingMachineFirmware/SMotor.cpp
@@ -0,0 +1,68 @@
+#include "config.h"
+#include "GCodeInterpreter.h"
+#include "SMotor.h"
+
+SMotor XStepper;
+SMotor YStepper;
+
+SMotor::SMotor( ){
+  
+  _abs_possition=0;
+  _enable=0;
+}
+
+SMotor::~SMotor(){}
+
+/* autor: Maciek Wiecheć
+  Inicjalizacja pinów
+*/
+
+void SMotor::Init(uint8_t DirPin, uint8_t StepPin, uint8_t EnPin){
+  _dir_pin=DirPin;
+  _step_pin=StepPin;
+  _enable_pin=EnPin;
+  pinMode(_dir_pin, OUTPUT);
+  pinMode(_step_pin, OUTPUT);
+  pinMode(_enable_pin, OUTPUT);
+  digitalWrite(_enable_pin, LOW);
+}
+
+/* autor: Maciek Wiecheć
+  Podgląd aktualniej pozycji w swojej osi
+*/
+long SMotor::GetAbsPossition(){
+  return _abs_possition;
+}
+
+/* autor: Maciek Wiecheć
+  Podgląd czy enable T/F
+*/
+bool SMotor::GetBoolEnable(){
+  return _enable;
+}
+
+
+/* autor: Maciek Wiecheć
+  Ustawianie Enable, nie wiem czy potrzebne ale jest
+*/
+void SMotor::SetEnable(bool EN){
+  _enable=EN;
+}
+
+
+/* autor: Maciek Wiecheć
+  Wykonanie pojedynczego kroku
+*/
+void SMotor::Step (int DIR){
+  
+  if(_enable){
+    digitalWrite(_enable_pin, HIGH);
+  }
+  else{
+    digitalWrite(_dir_pin, DIR=0? HIGH:LOW);
+    digitalWrite(_step_pin,HIGH);
+    digitalWrite(_step_pin,LOW);
+  }
+  
+}
+
---
+++ MillingMachineFirmware/SMotor.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <Arduino.h>
+#include "config.h"
+
+class SMotor
+{
+  private:
+    uint8_t _dir_pin;
+    uint8_t _step_pin;
+    uint8_t _enable_pin;
+    bool _enable;
+    long  _abs_possition;
+
+  public:
+
+    SMotor();
+    ~SMotor();
+    void Init(uint8_t ,uint8_t, uint8_t);
+    long GetAbsPossition();
+    bool GetBoolEnable();
+    void SetEnable(bool);
+    void Step (int DIR);
+};
+
+extern SMotor XStepper;
+extern SMotor YStepper;
